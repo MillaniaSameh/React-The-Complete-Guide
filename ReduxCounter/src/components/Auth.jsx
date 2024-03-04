e0c3a7193c03d984edd795b9c343c38096b08a31
Millania Sameh <samehmillania@gmail.com>
2024-03-04 13:56:21 +0200

    CounterDemo - Migrating to Redux Toolkit

--- ReduxCounter/src/components/Auth.jsx
+++ ReduxCounter/src/components/Auth.jsx
@@ -1,8 +1,19 @@
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth";
+
 const Auth = () => {
+  const dispatch = useDispatch();
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+
+    dispatch(authActions.login());
+  };
+
   return (
     <main className="auth">
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className="control">
             <label htmlFor="email">Email</label>
             <input type="email" id="email" />
